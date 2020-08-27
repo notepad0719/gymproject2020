294e032aae8eb4df604976f37a0898f0c976d5c7
notepad0719 <62315428+notepad0719@users.noreply.github.com>
2020-08-27 22:18:00 +0900

    readme update 0827

---
+++ test.js
@@ -0,0 +1,13 @@
+/*
+1. 행만들기 
+2. 열 만들기
+3. 테이블 요소 만들기
+*/
+
+// 1. 행 만들기
+
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+const table = document.createElement("table");
+
+console.log("tr");
