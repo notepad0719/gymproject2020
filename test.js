46c032ea1b3c240c986972598f87048702207f42
notepad0719 <62315428+notepad0719@users.noreply.github.com>
2020-08-30 01:55:14 +0900

    insertTableHeader - attendance.html
    table header - day count

--- test.js
+++ test.js
@@ -1,13 +1,29 @@
-/*
-1. 행만들기 
-2. 열 만들기
-3. 테이블 요소 만들기
-*/
+const tbl = document.createElement("table");
 
-// 1. 행 만들기
+const addedRow = [];
+const addedCell = [];
+const leapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const notLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-const tr = document.createElement("tr");
-const td = document.createElement("td");
-const table = document.createElement("table");
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
 
-console.log("tr");
+// 날짜는 +1 해줘야 한다. 왜? 나중에 mdn에서 정의 다시 살펴보자
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  year = leapYear;
+} else {
+  year = notLeapYear;
+}
+
+const dayCount = year[month - 1];
+
+for (let r = 0; r < 3; r++) {
+  addedRow[r] = document.createElement("tr");
+  tbl.appendChild(addedRow[r]);
+  for (let c = 0; c < dayCount; c++) {
+    addedCell[c] = document.createElement("td");
+    addedRow[r].appendChild(addedCell[c]);
+  }
+}
